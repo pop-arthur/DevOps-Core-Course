050f712366b5b9d4049ad9b7136f3d465cac027c
arthur <ar.popov@innopolis.university>
2026-02-18 20:08:29 +0300

    Lab 04 + Bonus

--- terraform/github.tf
+++ terraform/github.tf
@@ -3,13 +3,13 @@ provider "github" {
 }
 
 resource "github_repository" "course_repo" {
-  count         = var.create_github ? 1 : 0
-  name          = "DevOps-Core-Course"
-  description   = "🚀Production-grade DevOps course: 18 hands-on labs covering Docker, Kubernetes, Helm, Terraform, Ansible, CI/CD, GitOps (ArgoCD), monitoring (Prometheus/Grafana), and more. Build real-world skills with progressive delivery, secrets management, and cloud-native deployments. (Hi from terraform)"
-  visibility    = "public"
-  has_issues    = true
-  has_wiki      = true
-  has_projects  = true
+  count        = var.create_github ? 1 : 0
+  name         = "DevOps-Core-Course"
+  description  = "🚀Production-grade DevOps course: 18 hands-on labs covering Docker, Kubernetes, Helm, Terraform, Ansible, CI/CD, GitOps (ArgoCD), monitoring (Prometheus/Grafana), and more. Build real-world skills with progressive delivery, secrets management, and cloud-native deployments. (Hi from terraform)"
+  visibility   = "public"
+  has_issues   = true
+  has_wiki     = true
+  has_projects = true
 }
 
 variable "github_token" {
